c41797f54c222c2c99327196cd603efeef1290bc
Nils <nilsgrimmer@gmx.de>
2023-03-14 13:46:30 +0100

    Optimization

--- examples/colorOLED/colorOLED.ino
+++ examples/colorOLED/colorOLED.ino
@@ -6,13 +6,16 @@
 NGColorOLED co = NGColorOLED();
 
 void setup() {
+  char log[100];
   observeMemory(0);
   co.initialize();
   co.clear();
-  co.setScale(1);
+  co.setScale(5);
   co.beginUpdate();
   co.drawPoint(0, 0, COLOR_RED);
   co.endUpdate();
+  sprintf(log, "Width: %d Height: %d", co.getWidth(), co.getHeight());
+  Serial.println(log);
   observeMemory(0);
 }
 
