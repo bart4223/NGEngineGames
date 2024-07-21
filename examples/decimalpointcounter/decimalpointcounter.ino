2b2d720a5d517a92c6d03fe529a82bfb02e8c862
Nils <nilsgrimmer@gmx.de>
2024-07-21 16:28:27 +0200

    Optimizations

--- examples/decimalpointcounter/decimalpointcounter.ino
+++ examples/decimalpointcounter/decimalpointcounter.ino
@@ -1,4 +1,5 @@
-#define OLED //OLED, DOTMATRIX
+#define LEDSTRIP //OLED, DOTMATRIX, LEDSTRIP
+#define FONTDEFAULT //FONTDEFAULT, FONTZX81
 
 #include <NGMemoryObserver.h>
 #include <NGZX81Font.h>
@@ -7,6 +8,13 @@
 #endif
 #ifdef DOTMATRIX
 #include <NGColorDotMatrix.h>
+#endif
+#ifdef LEDSTRIP
+#include <NGColorLEDStrip.h>
+#define LEDSTRIPPIN           6
+#define LEDSTRIPPIXELS      100
+#define LEDSTRIPROWS         10
+#define LEDSTRIPBRIGHTNESS 0.05
 #endif
 #include <NGDecimalPointCounter.h>
 
@@ -20,8 +28,15 @@
 #define RANGE    1000
 #define DELAY   10000
 #endif
+#ifdef LEDSTRIP
+#define SCALE       1
+#define RANGE     100
+#define DELAY   10000
+#endif
 
+#ifdef FONTZX81
 NGZX81Font *fontZX81 = new NGZX81Font();
+#endif
 
 #ifdef OLED
 NGColorOLED *cdm = new NGColorOLED();
@@ -29,14 +44,24 @@ NGColorOLED *cdm = new NGColorOLED();
 #ifdef DOTMATRIX
 NGColorDotMatrix *cdm = new NGColorDotMatrix();
 #endif
+#ifdef LEDSTRIP
+NGColorLEDStrip *cdm = new NGColorLEDStrip(LEDSTRIPPIN, LEDSTRIPPIXELS, LEDSTRIPROWS);
+#endif
 NGDecimalPointCounter *dpc = new NGDecimalPointCounter(cdm, COLOR_RED);
 
+#define ANIMATIONDELAY 1000
+
 long _lastValue = 0;
+bool _init = true;
 
 void setup() {
   observeMemory(0);
   randomSeed(analogRead(A0));
-  cdm->initialize();
+  #ifdef LEDSTRIP
+  cdm->initialize(LEDSTRIPBRIGHTNESS);
+  #else
+  cdm.initialize();
+  #endif
   cdm->setScale(SCALE);
   cdm->clear();
   #ifdef OLED
@@ -46,9 +71,11 @@ void setup() {
   dpc->setMaxDigits(3);
   #endif
   dpc->setShowMaxDigits(false);
-  dpc->setIsAnimationEnabled(true);
-  dpc->setAnimationDelay(100);
+  //dpc->setIsAnimationEnabled(true);
+  dpc->setAnimationDelay(ANIMATIONDELAY);
+  #ifdef FONTZX81
   dpc->setFont(fontZX81);
+  #endif
   #ifdef OLED
   dpc->setPosX(5);
   dpc->setPosY(5);
@@ -58,15 +85,17 @@ void setup() {
   dpc->setPosY(0);
   #endif
   dpc->initialize();
-  dpc->setCounter(random(1, RANGE));
   observeMemory(0);
 }
 
 void loop() {
   dpc->processingLoop();
-  if (millis() - _lastValue > DELAY) {
+  if (millis() - _lastValue > DELAY | _init) {
+    _init = false;
     cdm->clear();
-    dpc->setCounter(random(1, RANGE));
+    int count = random(1, RANGE);
+    Serial.println(count);
+    dpc->setCounter(count);
     _lastValue = millis();
   }
   //dpc->incrementCounter(random(1, RANGE));
