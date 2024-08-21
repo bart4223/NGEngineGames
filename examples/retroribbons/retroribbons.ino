b58925a505a622fe7d0b444372fbc735b9363537
Nils <nilsgrimmer@gmx.de>
2024-08-21 07:43:55 +0200

    Colorribbons renamed

--- examples/retroribbons/retroribbons.ino
+++ examples/retroribbons/retroribbons.ino
@@ -1,22 +1,37 @@
+#define LEDSTRIP100 //LEDSTRIP100, LEDSTRIP256
+
 #include <NGMemoryObserver.h>
 #include <NGColorLEDStrip.h>
-#include <NGColorDotMatrixEffectRetroColors.h>
+#include <NGColorDotMatrixEffectRetroRibbons.h>
 
-#define LEDSTRIPPIN           6
+#ifdef LEDSTRIP256
+#define LEDSTRIPPIN           8
 #define LEDSTRIPPIXELS      256
 #define LEDSTRIPROWS         16
+#endif
+#ifdef LEDSTRIP100
+#define LEDSTRIPPIN           8
+#define LEDSTRIPPIXELS      100
+#define LEDSTRIPROWS         10
+#endif
 #define LEDSTRIPBRIGHTNESS 0.05
 
+#ifdef LEDSTRIP256
 NGColorLEDStrip *cdm = new NGColorLEDStrip(LEDSTRIPPIN, LEDSTRIPPIXELS, LEDSTRIPROWS, lskUpDownAlternate);
+#endif
+#ifdef LEDSTRIP100
+NGColorLEDStrip *cdm = new NGColorLEDStrip(LEDSTRIPPIN, LEDSTRIPPIXELS, LEDSTRIPROWS);
+#endif
 
-NGColorDotMatrixEffectRetroColors *rc = new NGColorDotMatrixEffectRetroColors(cdm);
+NGColorDotMatrixEffectRetroRibbons *effect = new NGColorDotMatrixEffectRetroRibbons(cdm);
 
 void setup() {
   observeMemory(0);
   cdm->initialize(LEDSTRIPBRIGHTNESS);
+  effect->initialize();
   observeMemory(0);
 }
 
 void loop() {
-  rc->processingLoop();
+  effect->processingLoop();
 }
