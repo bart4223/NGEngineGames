b8ffcb301bff5048cbda62ced7401b81b67efb48
Nils <nilsgrimmer@gmx.de>
2024-08-31 18:18:07 +0200

    Optimizations

--- examples/splash/splash.ino
+++ examples/splash/splash.ino
@@ -7,6 +7,8 @@
 #include <NGPaintableComponentEffectVoid.h>
 #include <NGColorDotMatrixEffectRetroRibbons.h>
 #include <NGColorDotMatrixEffectZini.h>
+#include <NGColorDotMatrixEffectText.h>
+#include <NGZX81Font.h>
 
 #ifdef LEDSTRIP256
 #define LEDSTRIPPIN           8
@@ -33,16 +35,22 @@ bool finished = false;
 
 NGSplash *splash = new NGSplash(new NGSerialNotification());
 NGColorDotMatrixEffectRetroRibbons *effectOne = new NGColorDotMatrixEffectRetroRibbons(cdm);
-NGColorDotMatrixEffectZini *effectTwo = new NGColorDotMatrixEffectZini(cdm);
-NGPaintableComponentEffectVoid *effectThree = new NGPaintableComponentEffectVoid(cdm);
+//NGColorDotMatrixEffectZini *effectTwo = new NGColorDotMatrixEffectZini(cdm);
+NGZX81Font *fontZX81 = new NGZX81Font();
+NGColorDotMatrixEffectText *effectThree = new NGColorDotMatrixEffectText(cdm, COLOR_BLUE, COLOR_TRANSPARENT, fontZX81);
+NGPaintableComponentEffectVoid *effectFour = new NGPaintableComponentEffectVoid(cdm);
 
 void setup() {
   observeMemory(0);
   cdm->initialize(LEDSTRIPBRIGHTNESS);
   splash->setLogging(LOGGING);
-  splash->registerEffect(effectOne, 0, 5000);
-  splash->registerEffect(effectTwo, 1000, 2000);
-  splash->registerEffect(effectThree, 5000, 10);
+  splash->registerEffect(effectOne, 0, 2400);
+  //splash->registerEffect(effectTwo, 1000, 2000);
+  effectThree->setPosition((cdm->getWidth() - 8) / 2, (cdm->getHeight() - 8) / 2);
+  effectThree->setDelay(200);
+  effectThree->setText("ARDCADE");
+  splash->registerEffect(effectThree, 500, 1400);
+  splash->registerEffect(effectFour, 2400, 10);
   splash->initialize();
   observeMemory(0);
 }
@@ -51,7 +59,9 @@ void loop() {
   if (!finished) {
     finished = splash->isFinished();
     if (!finished) {
+      cdm->beginUpdate();
       splash->processingLoop();
+      cdm->endUpdate();
     } else {
       splash->writeInfo("Splash finished!");
     }
